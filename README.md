988342e6e7f4d96cc17cf3c85a094df1a1503947
dpabon <13040959+dpabon@users.noreply.github.com>
2023-03-22 14:22:04 +0100

    Minor changes + runtest structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# YAXArraysToolbox
+# YAXArraysToolbox.jl
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://dpabon.github.io/YAXArraysToolbox.jl/stable/)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://dpabon.github.io/YAXArraysToolbox.jl/dev/)
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,23 @@ using Test
 
 @testset "YAXArraysToolbox.jl" begin
     # Write your tests here.
+
+    
+
+    # statistics by index
+
+
+
+    # space4time
+
+
+    # masking altitude
+
+
+    # masking using variables
+
+
+    # filling time variable using another cube time axis.
+
+    
 end
