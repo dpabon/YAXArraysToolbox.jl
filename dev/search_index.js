3d9a1a940028c55782ac2b76d62d57af92dc18de
Documenter.jl <documenter@juliadocs.github.io>
2023-03-22 08:47:38 +0000

    build based on 505acac

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = YAXArraysToolbox","category":"page"},{"location":"#YAXArraysToolbox","page":"Home","title":"YAXArraysToolbox","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for YAXArraysToolbox.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [YAXArraysToolbox]","category":"page"}]
+}
