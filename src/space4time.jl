14c88df1253122e104d9d2ea0a8782572ef9b8b3
dpabon <13040959+dpabon@users.noreply.github.com>
2023-10-02 10:40:22 +0200

    missing considered in the biophysical variable

--- src/space4time.jl
+++ src/space4time.jl
@@ -174,7 +174,9 @@ function s4time(
 
     #clim_var_cube_2 = permutedims(clim_var_cube_in, (3,2,1))
 
-    climvarmat = reshape(clim_var_cube_in, ((winsize^2), time_n))
+    climvarmat = collect(reshape(clim_var_cube_in, ((winsize^2), time_n)))
+
+    replace!(climvarmat, missing => NaN)
 
 
     local_pft1 =
