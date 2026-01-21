b007265fcda0745468d4d577fb7c57b9375aff6d
dpabon <13040959+dpabon@users.noreply.github.com>
2026-01-21 14:26:00 +0100

    Revert "test project created"
    This reverts commit d4eff0bf4073d44c449edb6395dd3c004f759652.

---
+++ test/runtests.jl
@@ -0,0 +1,25 @@
+using YAXArraysToolbox
+using Test
+
+@testset "YAXArraysToolbox.jl" begin
+    # Write your tests here.
+
+
+
+    # statistics by index
+
+
+
+    # space4time
+
+
+    # masking altitude
+
+
+    # masking using variables
+
+
+    # filling time variable using another cube time axis.
+
+
+end
